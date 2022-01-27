fceb02c2fae1bf47a15d9598c9382be3c439f39e
Alexanderç <Alexander@cesjuanpablosegundocadiz.es>
2022-01-27 16:00:41 +0100

    Agragamos una cadena y la mostramos en pantalla

--- ejercicioGit.java
+++ ejercicioGit.java
@@ -4,7 +4,8 @@ public class ejercicioGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String cadena = "Hola mundo";
+		System.out.println(cadena);
 	}
 
 }
