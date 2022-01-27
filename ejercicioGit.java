11d18e6231277de15486a49300da0619dc6e156b
Alexanderç <Alexander@cesjuanpablosegundocadiz.es>
2022-01-27 16:02:56 +0100

    Añadimos una nueva salida por pantalla con un msg

--- ejercicioGit.java
+++ ejercicioGit.java
@@ -6,6 +6,8 @@ public class ejercicioGit {
 		// TODO Auto-generated method stub
 		String cadena = "Hola mundo";
 		System.out.println(cadena);
+
+		System.out.println(cadena+" Viva GitHub");
 	}
 
 }
